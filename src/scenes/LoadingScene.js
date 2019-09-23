abeb705732d9e8c08f033ad9c3b99f0c83dcdb15
darkoverkill <maxym.fedas@gmail.com>
2019-09-23 21:46:06 +0300

    init, lessons 1-6

---
+++ src/scenes/LoadingScene.js
@@ -0,0 +1,47 @@
+class LoadingScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'LoadingScene' });
+  }
+
+  init(data) {
+    this.level_data = data.level_data;
+
+    let loading_message = this.add.text(
+      320,
+      240,
+      'Loading',
+      { font: '48px Minimal', fill: '#ffffff' }
+    );
+  }
+
+  preload() {
+    let assets = this.level_data.assets;
+    for (let assets_key in assets) {
+      let asset = assets[assets_key];
+      switch (asset.type) {
+        case 'image':
+          this.load.image(assets_key, asset.source);
+          break;
+        case 'spritesheet':
+          this.load.spritesheet(
+            assets_key,
+            asset.source,
+            {
+              frameWidth: asset.frame_width,
+              frameHeight: asset.frame_height,
+              frames: asset.frames,
+              margin: asset.margin,
+              spacing: asset.spacing
+            }
+          );
+          break;
+      }
+    }
+  }
+
+  create(data) {
+    console.log('starting title scene');
+  }
+}
+
+export default LoadingScene;
