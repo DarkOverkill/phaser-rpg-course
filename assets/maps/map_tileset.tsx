f96439276670af638425326888afddc50bbd15f8
darkoverkill <maxym.fedas@gmail.com>
2019-09-24 23:24:35 +0300

    lesson 9

---
+++ assets/maps/map_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="map_tileset" tilewidth="32" tileheight="32" tilecount="240" columns="20">
+ <image source="../images/tileset.png" width="640" height="384"/>
+</tileset>
